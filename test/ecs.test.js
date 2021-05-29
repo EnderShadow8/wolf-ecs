a8d1e29d8e4497ca0791f1d552e7fa5e7ef13bad
EnderShadow8 <69184571+EnderShadow8@users.noreply.github.com>
2021-05-29 22:50:59 +0800

    Add more tests

--- test/ecs.test.js
+++ test/ecs.test.js
@@ -9,7 +9,7 @@ describe("ECS", function() {
     ecs = new ECS()
   })
 
-  describe("#defineComponent", function() {
+  describe("defineComponent", function() {
     it("should initialise component pools as defined", function() {
       ecs.defineComponent("u16", types.u16)
       ecs.defineComponent("compound", {f32: types.f32, more: {f64: types.f64}})
@@ -33,11 +33,16 @@ describe("ECS", function() {
 
     it("should throw an error if _init === true", function() {
       ecs._init = true
-      expect(() => {ecs.defineComponent("wrong", types.u32)}).to.throw()
+      expect(() => ecs.defineComponent("wrong", types.u32)).to.throw()
+    })
+
+    it("should throw an error on duplicate names", function() {
+      ecs.defineComponent("wrong", types.u32)
+      expect(() => ecs.defineComponent("wrong", types.u32)).to.throw()
     })
   })
 
-  describe("#createQuery", function() {
+  describe("createQuery", function() {
     it("should return a query with appropriate mask", function() {
       ecs.defineComponent("cmp")
       for(let i = 0; i < 32; i++) {
@@ -61,12 +66,118 @@ describe("ECS", function() {
     })
 
     it("should throw an error on no arguments", function() {
-      expect(() => {ecs.createQuery()}).to.throw()
+      expect(() => ecs.createQuery()).to.throw()
     })
 
-    it("should throw an error on invalid names", function() {
+    it("should throw an error on invalid name", function() {
       ecs.defineComponent("right")
-      expect(() => {ecs.createQuery("wrong")}).to.throw()
+      expect(() => ecs.createQuery("wrong")).to.throw()
+    })
+  })
+
+  describe("createEntity", function() {
+    it("should increment the ID counter", function() {
+      expect(ecs.createEntity()).to.equal(0)
+      expect(ecs.createEntity()).to.equal(1)
+      expect(ecs.createEntity()).to.equal(2)
+    })
+
+    it("should use and remove recycled IDs from _rm", function() {
+      ecs.entID = 2
+      const arr = [0, 1]
+      ecs._rm = arr.slice()
+      const id = ecs.createEntity()
+
+      expect(arr).to.include(id)
+      expect(ecs._rm).to.not.include(id)
+    })
+
+    it("should set _empty to the empty archetype", function() {
+      ecs.defineComponent("cmp")
+      ecs.createEntity()
+      expect(ecs._empty).to.equal(ecs._arch.get("0"))
+    })
+
+    it("should set _empty to the empty archetype (n > 32)", function() {
+      for(let i = 0; i < 40; i++) {
+        ecs.defineComponent(i.toString())
+      }
+      ecs.createEntity()
+      expect(ecs._empty).to.equal(ecs._arch.get("0,0"))
+    })
+  })
+
+  describe("destroyEntity", function() {
+    it("should add ID to _rm", function() {
+      const id = ecs.createEntity()
+      ecs.destroyEntity(id)
+      expect(ecs._rm).to.have.members([0])
+
+      const id1 = ecs.createEntity()
+      const id2 = ecs.createEntity()
+      ecs.destroyEntity(id1)
+      ecs.destroyEntity(id2)
+      expect(ecs._rm).to.have.members([0, 1])
+    })
+
+    it("should remove entity from all archetypes", function() {
+      ecs.defineComponent("cmp")
+
+      const id = ecs.createEntity()
+      ecs.addComponent(id, "cmp")
+
+      ecs.destroyEntity(id)
+      expect(ecs._empty.has(id)).to.be.false
+      expect(ecs._arch.get("1").has(id)).to.be.false
+    })
+  })
+
+  describe("addComponent", function() {
+    it("should update archetype of entity", function() {
+      ecs.defineComponent("cmp1")
+      ecs.defineComponent("cmp2")
+      const id = ecs.createEntity()
+
+      ecs.addComponent(id, "cmp1")
+      expect(ecs._arch.get("0").has(id)).to.be.false
+      expect(ecs._arch.get("1").has(id)).to.be.true
+
+      ecs.addComponent(id, "cmp2")
+      expect(ecs._arch.get("0").has(id)).to.be.false
+      expect(ecs._arch.get("1").has(id)).to.be.false
+      expect(ecs._arch.get("3").has(id)).to.be.true
+    })
+
+    it("should throw an error on invalid name", function() {
+      const id = ecs.createEntity()
+      expect(() => ecs.addComponent(id, "wrong")).to.throw()
+    })
+  })
+
+  describe("removeComponent", function() {
+    it("should update archetype of entity", function() {
+      ecs.defineComponent("cmp1")
+      ecs.defineComponent("cmp2")
+      const id = ecs.createEntity()
+      ecs.addComponent(id, "cmp1")
+      ecs.addComponent(id, "cmp2")
+
+      ecs.removeComponent(id, "cmp1")
+      expect(ecs._arch.get("0").has(id)).to.be.false
+      expect(ecs._arch.get("1").has(id)).to.be.false
+      expect(ecs._arch.get("2").has(id)).to.be.true
+      expect(ecs._arch.get("3").has(id)).to.be.false
+
+      ecs.removeComponent(id, "cmp2")
+      expect(ecs._arch.get("0").has(id)).to.be.true
+      expect(ecs._arch.get("1").has(id)).to.be.false
+      expect(ecs._arch.get("2").has(id)).to.be.false
+      expect(ecs._arch.get("3").has(id)).to.be.false
+    })
+
+    it("should throw an error on invalid name", function() {
+      const id = ecs.createEntity()
+      expect(() => ecs.removeComponent(id, "wrong")).to.throw()
     })
   })
 })
