6087693fc8d5881e154945938c27c43723f611d9
EnderShadow8 <andyhanwu@gmail.com>
2021-06-18 16:45:48 +0800

    Add setup function to system

--- src/system.ts
+++ src/system.ts
@@ -1,7 +1,8 @@
 import {Query} from "./query"
 
-function defineSystem(query: Query, func: (id: number) => void, cleanup?: () => void) {
+function defineSystem(query: Query, func: (id: number) => void, setup?: () => void, cleanup?: () => void) {
   return function() {
+    setup?.()
     for(let i = 0, l = query.archetypes.length; i < l; i++) {
       const ent = query.archetypes[i].entities
       for(let j = ent.length; j > 0; j--) {
